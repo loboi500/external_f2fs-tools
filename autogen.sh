bb431550c804cc70bdc59f8691a87577985c3f85
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 17:15:14 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,3 @@
-#!/bin/sh
-
-aclocal && \
-autoheader && \
-autoconf && \
-libtoolize && \
-automake -a -c
+version https://git-lfs.github.com/spec/v1
+oid sha256:16715db7d23e96c1e002425c10c78734114d8e04c88191e58d22350c78941764
+size 85
